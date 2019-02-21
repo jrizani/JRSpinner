b9ce58c437173a0259ff78cfeb3108fb0917b679
julian noor rizani <juliannoorrizani@gmail.com>
2019-02-21 10:47:41 +0700

    update example

--- example/src/main/java/jrizani/jrspinner/example/MainActivity.java
+++ example/src/main/java/jrizani/jrspinner/example/MainActivity.java
@@ -2,6 +2,8 @@ package jrizani.jrspinner.example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import jrizani.jrspinner.JRSpinner;
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private JRSpinner mJRSpinner;
     private TextView mSelectedIndex;
+    private Button mBtnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mJRSpinner = findViewById(R.id.JRSpinner);
         mSelectedIndex = findViewById(R.id.selectedIndex);
+        mBtnReset = findViewById(R.id.btn_reset);
 
         mJRSpinner.setItems(getResources().getStringArray(R.array.tesItems));
         mJRSpinner.setTitle("Choose jrspinner_item programmatically");
@@ -26,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(int position) {
                 mSelectedIndex.setText(position + "");
+            }
+        });
+
+        mBtnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mJRSpinner.clear();
+                mSelectedIndex.setText("");
             }
         });
     }
